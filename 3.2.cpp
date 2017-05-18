fb9be7676318d02fb64bfbcba9476a99ab1a37c4
TesLA_422 <tesla422zxc@outlook.com>
2017-05-18 13:54:51 +0800

    Update 3.2.cpp

--- 3.2.cpp
+++ 3.2.cpp
@@ -1,5 +1,5 @@
 /*
- *Q3.2 How would you design a stack which,in addition to push and pop,also has a function min which returns the minimu element?
+ *Q3.2 How would you design a stack which,in addition to push and pop,also has a function min which returns the minimum element?
  *Push,pop and min should all oprate in O(1) time.
  */
 
