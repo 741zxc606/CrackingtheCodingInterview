c22b2d8b04fe61dbce04e3e7c9afd1d988ac841d
TesLA_422 <tesla422zxc@outlook.com>
2017-05-30 20:07:05 +0800

    Update 19.10.cpp

--- Chapter 19 Moderate/19.10.cpp
+++ Chapter 19 Moderate/19.10.cpp
@@ -1,3 +1,8 @@
+/*
+ * Q 19.10 Write a method to generate a random number between 1 and 7,
+ * given a method that generates a random number between 1 and 5(i.e.,implement rand7()using rand5()).
+ */
+
 #include <iostream>
 using namespace std;
 
