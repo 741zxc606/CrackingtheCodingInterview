017ce0217c19808ada3e6d2251a1e7711c099d47
TesLA_422 <tesla422zxc@outlook.com>
2017-06-05 09:55:28 +0800

    Create 13.6.cpp

---
+++ Chapter 13 CPP/13.6.cpp
@@ -0,0 +1,29 @@
+/*
+ * 13.6
+ */
+
+#include <iostream>
+using namespace std;
+
+class FirstClass {
+public:
+	virtual void MethodA(int) {
+		cout << "ForstClass::MethodA(int)" << endl;
+	}
+	virtual void MethodA(int, int) {
+		cout << "FirstClass::MethodA(int,int)" << endl;
+	}
+};
+
+class SecondClass :public FirstClass {
+public:
+	void MethodA1(int, int, int) {
+		cout << "SecondClass::MethodA(int)" << endl;
+	}
+};
+
+int main() {
+	SecondClass sc;
+	sc.MethodA(0,0);
+	return 0;
+}
